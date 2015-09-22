bc9c6f80902f11a2783e8546060dad492d4965e4
rlacroix <rlacroix@d613d254-d18a-4304-b296-5125d4c4d670>
2015-09-22 08:09:53 +0000

    Registry: Fix the getter/setter methods.
     - Fix a typo in `void setKey(const std::string& key, const CBaseType& value)`.
     - Fix the templated methods by taking advantage of SFINAE concept.
    
    git-svn-id: https://forge.ipsl.fr/ioserver/svn/XIOS/trunk@700 d613d254-d18a-4304-b296-5125d4c4d670

--- src/registry.hpp
+++ src/registry.hpp
@@ -6,12 +6,16 @@
 #include "mpi.hpp"
 #include "message.hpp"
 
+// Those two headers can be replaced by the C++11 equivalent in the future
+#include <boost/utility/enable_if.hpp>
+#include <boost/type_traits.hpp>
+
 namespace xios
 {
 /*!
   \class CRegistry
- This class is a registry database which store key with an associated value. Internaly the value is stored as a memory bloc
- and the key is a string. The registry can be gathered and merge between mpi process, broadcasted and read or wrote from a file
+ This class is a registry database which store key with an associated value. Internally the value is stored as a memory bloc
+ and the key is a string. The registry can be gathered and merge between MPI process, broadcast and read or wrote from a file
 */
   class CRegistry : virtual public CBaseType
   {
@@ -19,28 +23,30 @@ namespace xios
 
 /** Constructor, the communicator is used for bcast or gather operation between MPI processes */
       CRegistry(const MPI_Comm& comm=MPI_COMM_WORLD) : communicator(comm) {}
-      
+
 /** Copy constructor */
       CRegistry(const CRegistry& reg) ;
-      
+
 
 /** insert a value associated to a key*/
-      void setKey(const std::string& key, const CBaseType& value) {this->setKey(key,value); }
+      void setKey(const std::string& key, const CBaseType& value) { this->setKey_(key,value); }
 
 /** insert a value associated to a key*/
-      template<typename T> void setKey(const std::string& key, const CType<T>& value) {this->setKey_(key,value); }
+      template<typename T> typename boost::enable_if_c<!boost::is_convertible<T&, CBaseType&>::value>::type
+      setKey(const std::string& key, const T& value) { this->setKey_(key,CType<T>(value)); }
 
 
-/** retreive a value from a key */
-      void getKey(const std::string& key, CBaseType& value ) {this->getKey_(key,value); }
+/** retrieve a value from a key */
+      void getKey(const std::string& key, CBaseType& value) { this->getKey_(key,value); }
 
-/** retreive a value from a key */
-      template<typename T> void getKey(const std::string& key, CType<T>&  value ) {this->getKey_(key,value); }
+/** retrieve a value from a key */
+      template<typename T> typename boost::enable_if_c<!boost::is_convertible<T&, CBaseType&>::value>::type
+      getKey(const std::string& key, T& value) { CType_ref<T> valRef(value); this->getKey_(key,valRef); }
 
 
 /** query for an already inserted key */
       bool foundKey(const std::string& key) const ;
-      
+
 /** The registry is wrote into a memory buffer */
       bool toBuffer(CBufferOut& buffer) const ;
 
@@ -50,20 +56,20 @@ namespace xios
 /** The registry is wrote to the file given by "filename". If the registry is empty no file is wrote */
       void toFile(const string& filename) ;
 
-/** The registry is read from the file given by "filename". If no file exist, the registry remain empty */     
+/** The registry is read from the file given by "filename". If no file exist, the registry remain empty */
       void fromFile(const string& filename) ;
 
-/** Merge the registry with an other. Existing keys in the current registry are not overwritten */         
+/** Merge the registry with an other. Existing keys in the current registry are not overwritten */
       void mergeRegistry(const CRegistry& inRegistry) ;
 
-/** Broadcast registry from the root process (rank 0) to the other processes of the communicator */    
+/** Broadcast registry from the root process (rank 0) to the other processes of the communicator */
       void bcastRegistry(void) ;
 
-/** Gather registry to the root process (rank 0) from the other processes of the communicator */          
+/** Gather registry to the root process (rank 0) from the other processes of the communicator */
       void gatherRegistry(void) ;
 
-/** Gather registry with a hierachical algorithm which avoid root process to get registries from whole processes of the communicator.
-   Registry are merged two by two hirarchicaly. */
+/** Gather registry with a hierarchical algorithm which avoid root process to get registries from whole processes of the communicator.
+   Registry are merged two by two hierarchically. */
       void hierarchicalGatherRegistry(void) ;
 
 /** Destructor */
@@ -72,32 +78,32 @@ namespace xios
 /** Unimplemented, do not use (need for CBaseType pure virtual class) */
       void fromString(const string& str) ;
 
-/** Dump registry to a string (need for CBaseType pure virtual class)*/      
+/** Dump registry to a string (need for CBaseType pure virtual class)*/
       string toString(void) const ;
 
-/** Clone the registry (need for CBaseType pure virtual class)*/      
+/** Clone the registry (need for CBaseType pure virtual class)*/
       CRegistry* clone(void) const { return new CRegistry(*this); }
 
-/** return the size needed to bufferized the registry (need for CBaseType pure virtual class)*/        
+/** return the size needed to bufferize the registry (need for CBaseType pure virtual class)*/
       size_t size(void) const ;
 
-/** return true if the registry is empty (need for CBaseType pure virtual class)*/ 
+/** return true if the registry is empty (need for CBaseType pure virtual class)*/
       bool isEmpty(void) const { return registry.empty(); }
 
-/** Clean the registry and delete associated memory (need for CBaseType pure virtual class)*/ 
+/** Clean the registry and delete associated memory (need for CBaseType pure virtual class)*/
       void reset(void) ;
 
-/** Set the prefix added systematicaly to the keys, with "::" as separator*/       
+/** Set the prefix added systematically to the keys, with "::" as separator*/
       void setPath(const string& str) { path=str+"::" ; }
-       
+
     private:
 
 /** insert a value associated to a key (internal use)*/
       void setKey_(const std::string& key, const CBaseType& value) ;
 
-/** retreive a value from a key (internal use)*/
-      void getKey_(const std::string& key, CBaseType& value ) ;
-      
+/** retrieve a value from a key (internal use)*/
+      void getKey_(const std::string& key, CBaseType& value) ;
+
 /** use internally for recursivity */
       void gatherRegistry(const MPI_Comm& comm) ;
 
@@ -105,11 +111,11 @@ namespace xios
       void hierarchicalGatherRegistry(const MPI_Comm& comm) ;
 
 
-/** Prefix added systematicaly to the keys, with "::" as separator*/
+/** Prefix added systematically to the keys, with "::" as separator*/
       std::string path ;
 
 /** Map containing registry, the key is a string type and the value is stored in a pair with the size
- *  of the memory bloc and the associated pointer*/      
+ *  of the memory bloc and the associated pointer*/
       std::map<std::string,std::pair<size_t,char*> > registry ;
 
 /** MPI communicator used for broadcast and gather operation */
