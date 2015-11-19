af42e2f28badef0b1e841c009e296d8600dd1c8e
rlacroix <rlacroix@d613d254-d18a-4304-b296-5125d4c4d670>
2015-11-19 15:55:16 +0000

    Fortran interface: Make the compilation of some helper functions more resiliant.
    git-svn-id: https://forge.ipsl.fr/ioserver/svn/XIOS/trunk@793 d613d254-d18a-4304-b296-5125d4c4d670

--- src/interface/c/icutil.hpp
+++ src/interface/c/icutil.hpp
@@ -7,62 +7,38 @@
 #define __ICUTIL_HPP__
 
 #include <string>
-#include <string.h>
+#include <algorithm>
+
 // ///////////////////////// Définitions/Déclarations /////////////////////// //
 
-inline bool cstr2string(const char * cstr, int cstr_size, std::string & str)
+inline bool cstr2string(const char* cstr, int cstr_size, std::string& str)
 {
-  std::string valtemp;
-  std::size_t d, f = 0;
-  if (cstr_size != -1) 
+  if (cstr_size != -1)
   { 
-     valtemp.append (cstr, cstr_size);
-     d = valtemp.find_first_not_of(' ');
-     f = valtemp.find_last_not_of (' ');
-     str = valtemp.substr(d, f-d+1); 
-     return (true);
+    std::string valtemp;
+    std::size_t d, f = 0;
+
+    valtemp.append(cstr, cstr_size);
+    d = valtemp.find_first_not_of(' ');
+    f = valtemp.find_last_not_of (' ');
+    str = valtemp.substr(d, f - d + 1);
+
+    return true;
   }
   else
-  {
-     return (false);
-  }  
+     return false;
 }
 
-inline bool string_copy(const string& str, char* cstr,int cstr_size)
+inline bool string_copy(const std::string& str, char* cstr, int cstr_size)
 {
-  
-  if (str.size()>cstr_size) return false ;
+  if (str.size() > cstr_size)
+    return false;
   else
   {
-    std::memset (cstr,' ',cstr_size);
-    str.copy(cstr,cstr_size) ;
-    return true ;
+    std::fill(cstr, cstr + cstr_size, ' ');
+    str.copy(cstr, cstr_size);
+    return true;
   }
 }
-/*
-  template<class T>
-  inline  bool array_copy(ARRAY(T,1) array_in, T* array_out, size_t extent1)
-  {
-    if (array_in->num_elements() != extent1) return false ;
-    std::copy(array_in->data(), array_in->data() + array_in->num_elements(), array_out);
-    return true ;
-  }
-
-  template<class T>
-  inline  bool array_copy(ARRAY(T,2) array_in, T* array_out, size_t extent1, size_t extent2)
-  {
-    if (array_in->num_elements() != extent1*extent2) return false ;
-    std::copy(array_in->data(), array_in->data() + array_in->num_elements(), array_out);
-    return true ;
-  }
-
-  template<class T>
-  inline  bool array_copy(ARRAY(T,3) array_in, T* array_out, size_t extent1, size_t extent2, size_t extent3)
-  {
-    if (array_in->num_elements() != extent1*extent2*extent3) return false ;
-    std::copy(array_in->data(), array_in->data() + array_in->num_elements(), array_out);
-    return true ;
-  }        
-*/
 
 #endif // __ICUTIL_HPP__
