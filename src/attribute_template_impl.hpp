mo-marqh/XIOS-repository-migration-experiments

commit 42a0eb59fb23ccefe3e350384d108f06f1f03709

Check if output_frequency >= timestep added. Files with output_frequency < timestep are considered to be disabled.
git-svn-id: https://forge.ipsl.fr/ioserver/svn/XIOS/trunk@1015 d613d254-d18a-4304-b296-5125d4c4d670

src/duration.cpp
@@ -25,6 +25,13 @@ namespace xios
         return *this;
       }
 
+      bool operator<(CDuration a, CDuration b)
+      {
+        double aSeconds = a.second + 60.*(a.minute + 60.*(a.hour + 24.*(a.day + a.year*365.25) ) );
+        double bSeconds = b.second + 60.*(b.minute + 60.*(b.hour + 24.*(b.day + b.year*365.25) ) );
+        return (aSeconds < bSeconds);
+      }
+
       StdOStream& operator<<(StdOStream& out, const CDuration& duration)
       {
          out << duration.toString();

src/duration.hpp
@@ -22,6 +22,8 @@ namespace xios
             /// Opérateurs ///
             CDuration& operator=(const CDuration& duration);
 
+            friend bool operator<(CDuration a, CDuration b);
+
             friend StdOStream& operator<<(StdOStream& out, const CDuration& duration);
             friend StdIStream& operator>>(StdIStream& in , CDuration& duration);
 

src/node/context.cpp
@@ -570,10 +570,28 @@ namespace xios {
          if (!allFiles[i]->enabled.isEmpty()) // Si l'attribut 'enabled' est défini.
          {
             if (allFiles[i]->enabled.getValue()) // Si l'attribut 'enabled' est fixé à vrai.
+            {
+              if ( allFiles[i]->output_freq.getValue() < this->getCalendar()->getTimeStep())
+              {
+                error(0)<<"WARNING: void CContext::findEnabledFiles()"<<endl
+                    << "Output frequency in file \""<<allFiles[i]->getId()
+                    <<"\" is greater than the time step. File will not be written."<<endl;
+              }
+              else
                enabledFiles.push_back(allFiles[i]);
+            }
          }
-         else enabledFiles.push_back(allFiles[i]); // otherwise true by default
-
+         else
+         {
+           if ( allFiles[i]->output_freq.getValue() < this->getCalendar()->getTimeStep())
+           {
+             error(0)<<"WARNING: void CContext::findEnabledFiles()"<<endl
+                 << "Output frequency in file \""<<allFiles[i]->getId()
+                 <<"\" is greater than the time step. File will not be written."<<endl;
+           }
+           else
+             enabledFiles.push_back(allFiles[i]); // otherwise true by default
+         }
 
       if (enabledFiles.size() == 0)
          DEBUG(<<"Aucun fichier ne va être sorti dans le contexte nommé \""

commit 04521c5f9287312a0c596f7f9079285cc7e7891d

Correcting a bug introduced in r1015 with a check if output_frequency >= timestep.
git-svn-id: https://forge.ipsl.fr/ioserver/svn/XIOS/trunk@1028 d613d254-d18a-4304-b296-5125d4c4d670

src/attribute_template_impl.hpp
@@ -70,15 +70,15 @@ namespace xios
       template <class T>
          T CAttributeTemplate<T>::getValue(void) const
       {
-         return CType<T>::get() ;
+        return CType<T>::get() ;
 /*
-         if (SuperClass::isEmpty())
-         {
-            ERROR("T CAttributeTemplate<T>::getValue(void) const",
-                  << "[ id = " << this->getId() << "]"
-                  << " L'attribut est requis mais n'est pas défini !");
-          }
-         return (SuperClass::getValue<T>());
+        if (SuperClass::isEmpty())
+        {
+           ERROR("T CAttributeTemplate<T>::getValue(void) const",
+                 << "[ id = " << this->getId() << "]"
+                 << " L'attribut est requis mais n'est pas défini !");
+         }
+        return (SuperClass::getValue<T>());
 */
       }
 

src/duration.cpp
@@ -25,13 +25,6 @@ namespace xios
         return *this;
       }
 
-      bool operator<(CDuration a, CDuration b)
-      {
-        double aSeconds = a.second + 60.*(a.minute + 60.*(a.hour + 24.*(a.day + a.year*365.25) ) );
-        double bSeconds = b.second + 60.*(b.minute + 60.*(b.hour + 24.*(b.day + b.year*365.25) ) );
-        return (aSeconds < bSeconds);
-      }
-
       StdOStream& operator<<(StdOStream& out, const CDuration& duration)
       {
          out << duration.toString();

src/duration.hpp
@@ -22,8 +22,6 @@ namespace xios
             /// Opérateurs ///
             CDuration& operator=(const CDuration& duration);
 
-            friend bool operator<(CDuration a, CDuration b);
-
             friend StdOStream& operator<<(StdOStream& out, const CDuration& duration);
             friend StdIStream& operator>>(StdIStream& in , CDuration& duration);
 

src/node/context.cpp
@@ -565,17 +565,18 @@ namespace xios {
    void CContext::findEnabledFiles(void)
    {
       const std::vector<CFile*> allFiles = CFile::getAll();
+      const CDate& initDate = calendar->getInitDate();
 
       for (unsigned int i = 0; i < allFiles.size(); i++)
          if (!allFiles[i]->enabled.isEmpty()) // Si l'attribut 'enabled' est défini.
          {
             if (allFiles[i]->enabled.getValue()) // Si l'attribut 'enabled' est fixé à vrai.
             {
-              if ( allFiles[i]->output_freq.getValue() < this->getCalendar()->getTimeStep())
+              if ((initDate + allFiles[i]->output_freq.getValue()) < (initDate + this->getCalendar()->getTimeStep()))
               {
                 error(0)<<"WARNING: void CContext::findEnabledFiles()"<<endl
-                    << "Output frequency in file \""<<allFiles[i]->getId()
-                    <<"\" is greater than the time step. File will not be written."<<endl;
+                    << "Output frequency in file \""<<allFiles[i]->getFileOutputName()
+                    <<"\" is less than the time step. File will not be written."<<endl;
               }
               else
                enabledFiles.push_back(allFiles[i]);
@@ -583,11 +584,11 @@ namespace xios {
          }
          else
          {
-           if ( allFiles[i]->output_freq.getValue() < this->getCalendar()->getTimeStep())
+           if ( (initDate + allFiles[i]->output_freq.getValue()) < (initDate + this->getCalendar()->getTimeStep()))
            {
              error(0)<<"WARNING: void CContext::findEnabledFiles()"<<endl
-                 << "Output frequency in file \""<<allFiles[i]->getId()
-                 <<"\" is greater than the time step. File will not be written."<<endl;
+                 << "Output frequency in file \""<<allFiles[i]->getFileOutputName()
+                 <<"\" is less than the time step. File will not be written."<<endl;
            }
            else
              enabledFiles.push_back(allFiles[i]); // otherwise true by default