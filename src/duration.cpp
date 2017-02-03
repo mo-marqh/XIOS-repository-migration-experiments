bde5c8de9dcd4d15b4c534a5ad8b6399168f8722
rlacroix <rlacroix@d613d254-d18a-4304-b296-5125d4c4d670>
2017-02-03 13:26:51 +0000

    Fix: Do not allow redefining the same duration unit.
    For example "4d 7d" used to be parsed as "7d", now an error will be thrown.
    
    Fixes ticket #115.
    
    git-svn-id: https://forge.ipsl.fr/ioserver/svn/XIOS/trunk@1049 d613d254-d18a-4304-b296-5125d4c4d670

--- src/duration.cpp
+++ src/duration.cpp
@@ -37,6 +37,17 @@ namespace xios
         double v = 1.0;
         char   c = '/';
         bool   invalidUnit = false;
+        CDuration sentinel = NoneDu;
+
+#define setDuration(unit, value)                                                  \
+        {                                                                         \
+          if (sentinel.unit)                                                      \
+            ERROR("StdIStream& operator>>(StdIStream& in , CDuration& duration)", \
+                  << "Bad duration format: " #unit " has already been set.");      \
+                                                                                  \
+          duration.unit = value;                                                  \
+          sentinel.unit = 1.0;                                                    \
+        }
 
         do
         {
@@ -47,22 +58,22 @@ namespace xios
 
           switch (c)
           {
-            case 'y': duration.year   = v; break;
-            case 'd': duration.day    = v; break;
-            case 'h': duration.hour   = v; break;
-            case 's': duration.second = v; break;
+            case 'y': setDuration(year, v) break;
+            case 'd': setDuration(day, v) break;
+            case 'h': setDuration(hour, v) break;
+            case 's': setDuration(second, v) break;
             case 'm':
             {
               in >> c;
-              if      (c == 'i') duration.minute = v;
-              else if (c == 'o') duration.month  = v;
+              if      (c == 'i') setDuration(minute, v)
+              else if (c == 'o') setDuration(month, v)
               else invalidUnit = true;
               break;
             }
             case 't':
             {
               in >> c;
-              if (c == 's') duration.timestep = v;
+              if (c == 's') setDuration(timestep, v)
               else invalidUnit = true;
               break;
             }
@@ -75,6 +86,8 @@ namespace xios
             ERROR("StdIStream& operator>>(StdIStream& in , CDuration& duration)",
                   << "Bad duration format: invalid unit, unexpected '" << c << "' character.");
         } while (in.peek() != EOF); // check whether there is a next character to read
+
+#undef setDuration
 
         return in;
       }
