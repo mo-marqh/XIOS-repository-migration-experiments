e78eebd5dd0e55c30f645e198b9edef877d63591
oabramkina <oabramkina@d613d254-d18a-4304-b296-5125d4c4d670>
2017-08-29 15:18:53 +0000

    Fixing a bug appeared in dev after merging with trunk. Behavior in case of activated missing_values needs to be checked further.
    git-svn-id: https://forge.ipsl.fr/ioserver/svn/XIOS/dev/XIOS_DEV_CMIP6@1248 d613d254-d18a-4304-b296-5125d4c4d670

--- src/node/field.cpp
+++ src/node/field.cpp
@@ -1023,15 +1023,18 @@ namespace xios{
        // If the field data is to be read by the client or/and written to a file
        if (enableOutput && !storeFilter && !fileWriterFilter)
        {
-         storeFilter = boost::shared_ptr<CStoreFilter>(new CStoreFilter(gc, CContext::getCurrent(), grid,
-                                                                        detectMissingValues, defaultValue));
-         instantDataFilter->connectOutput(storeFilter, 0);
-       }
+         if (!read_access.isEmpty() && read_access)
+         {
+           storeFilter = boost::shared_ptr<CStoreFilter>(new CStoreFilter(gc, CContext::getCurrent(), grid,
+                                                                          detectMissingValues, defaultValue));
+           instantDataFilter->connectOutput(storeFilter, 0);
+         }
 
-       if (file && (file->mode.isEmpty() || file->mode == CFile::mode_attr::write))
-       {
-         fileWriterFilter = boost::shared_ptr<CFileWriterFilter>(new CFileWriterFilter(gc, this));
-         getTemporalDataFilter(gc, file->output_freq)->connectOutput(fileWriterFilter, 0);
+         if (file && (file->mode.isEmpty() || file->mode == CFile::mode_attr::write))
+         {
+           fileWriterFilter = boost::shared_ptr<CFileWriterFilter>(new CFileWriterFilter(gc, this));
+           getTemporalDataFilter(gc, file->output_freq)->connectOutput(fileWriterFilter, 0);
+         }
        }
      }
    }
