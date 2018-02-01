4fb8be84367cc57764ee2c6f638feab0247ffd81
ymipsl <ymipsl@d613d254-d18a-4304-b296-5125d4c4d670>
2018-02-01 08:41:34 +0000

    Bug fix when writing axis coordinate in some specific cases.
    YM
    
    git-svn-id: https://forge.ipsl.fr/ioserver/svn/XIOS/dev/XIOS_DEV_CMIP6@1416 d613d254-d18a-4304-b296-5125d4c4d670

--- src/node/axis.cpp
+++ src/node/axis.cpp
@@ -1091,7 +1091,8 @@ namespace xios {
     // Some value should be reset here
     data_begin.setValue(0);
     globalLocalIndexMap_.rehash(std::ceil(index.numElements()/globalLocalIndexMap_.max_load_factor()));
-    for (int idx = 0; idx < index.numElements(); ++idx) globalLocalIndexMap_[idx] = index(idx);
+//    for (int idx = 0; idx < index.numElements(); ++idx) globalLocalIndexMap_[idx] = index(idx);
+    for (int idx = 0; idx < index.numElements(); ++idx) globalLocalIndexMap_[index(idx)] = idx;
   }
 
   /*
