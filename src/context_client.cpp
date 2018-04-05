df07cb457c65a605e936b27bec6193843ac7af10
ymipsl <ymipsl@d613d254-d18a-4304-b296-5125d4c4d670>
2018-04-05 11:32:02 +0000

    Change MPI_UINT64_T by MPI_LONG_LONG_INT since MPI_UINT64_T has been standardize only by MPI3 norm.
    YM
    
    git-svn-id: https://forge.ipsl.fr/ioserver/svn/XIOS/dev/XIOS_DEV_CMIP6@1475 d613d254-d18a-4304-b296-5125d4c4d670

--- src/context_client.cpp
+++ src/context_client.cpp
@@ -100,7 +100,8 @@ namespace xios
         int typeId, classId, typeId_in, classId_in, timeLine_out;
         typeId_in=event.getTypeId() ;
         classId_in=event.getClassId() ;
-        MPI_Allreduce(&timeLine,&timeLine_out, 1, MPI_UINT64_T, MPI_SUM, intraComm) ;
+//        MPI_Allreduce(&timeLine,&timeLine_out, 1, MPI_UINT64_T, MPI_SUM, intraComm) ; // MPI_UINT64_T standardized by MPI 3
+        MPI_Allreduce(&timeLine,&timeLine_out, 1, MPI_LONG_LONG_INT, MPI_SUM, intraComm) ; 
         MPI_Allreduce(&typeId_in,&typeId, 1, MPI_INT, MPI_SUM, intraComm) ;
         MPI_Allreduce(&classId_in,&classId, 1, MPI_INT, MPI_SUM, intraComm) ;
         if (typeId/clientSize!=event.getTypeId() || classId/clientSize!=event.getClassId() || timeLine_out/clientSize!=timeLine)
