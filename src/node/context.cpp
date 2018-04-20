a12229ba92781ba0f1cbd20e1c70d389c2e85d9d
oabramkina <oabramkina@d613d254-d18a-4304-b296-5125d4c4d670>
2018-04-20 15:04:38 +0000

    Trunk and CMIP6: adding a check on output_freq attribute. Exception is thrown if it is empty.
    git-svn-id: https://forge.ipsl.fr/ioserver/svn/XIOS/trunk@1488 d613d254-d18a-4304-b296-5125d4c4d670

--- src/node/context.cpp
+++ src/node/context.cpp
@@ -602,6 +602,12 @@ namespace xios {
          {
             if (allFiles[i]->enabled.getValue()) // Si l'attribut 'enabled' est fixé à vrai.
             {
+              if (allFiles[i]->output_freq.isEmpty())
+              {
+                 ERROR("CContext::findEnabledFiles()",
+                     << "Mandatory attribute output_freq must be defined for file \""<<allFiles[i]->getFileOutputName()
+                     <<" \".")
+              }
               if ((initDate + allFiles[i]->output_freq.getValue()) < (initDate + this->getCalendar()->getTimeStep()))
               {
                 error(0)<<"WARNING: void CContext::findEnabledFiles()"<<endl
@@ -614,6 +620,12 @@ namespace xios {
          }
          else
          {
+           if (allFiles[i]->output_freq.isEmpty())
+           {
+              ERROR("CContext::findEnabledFiles()",
+                  << "Mandatory attribute output_freq must be defined for file \""<<allFiles[i]->getFileOutputName()
+                  <<" \".")
+           }  
            if ( (initDate + allFiles[i]->output_freq.getValue()) < (initDate + this->getCalendar()->getTimeStep()))
            {
              error(0)<<"WARNING: void CContext::findEnabledFiles()"<<endl
