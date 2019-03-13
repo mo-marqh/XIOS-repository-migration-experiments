62b713c0e55c014c1492b03c575a8cbb3ecac68c
ymipsl <ymipsl@d613d254-d18a-4304-b296-5125d4c4d670>
2019-03-13 16:15:18 +0000

    Implement tracing with scorep
    YM
    
    git-svn-id: https://forge.ipsl.fr/ioserver/svn/XIOS/trunk@1655 d613d254-d18a-4304-b296-5125d4c4d670

--- src/tracer.cpp
+++ src/tracer.cpp
@@ -1,7 +1,15 @@
 #include "tracer.hpp"
-#ifdef VTRACE
+
+#if defined(VTRACE)
+
 #include <vt_user.h>
+
+#elif defined(SCOREP)
+
+#include <scorep/SCOREP_User.h>
+
 #endif
+
 #include <string>
 
 namespace xios
@@ -10,29 +18,37 @@ namespace xios
   
   void traceOn(void)
   {
-#ifdef VTRACE
+#if defined(VTRACE)
     VT_ON() ;
+#elif defined(SCOREP)
+    SCOREP_RECORDING_ON()
 #endif
   }
   
   void traceOff(void) 
   {
-#ifdef VTRACE
+#if defined(VTRACE)
     VT_OFF() ;
+#elif defined(SCOREP)
+    SCOREP_RECORDING_OFF()
 #endif
   }
   
   void traceBegin(const string& name)
   {
-#ifdef VTRACE
+#if defined(VTRACE)
     VT_USER_START(name.c_str()) ;
+#elif defined(SCOREP)
+    SCOREP_USER_REGION_BY_NAME_BEGIN(name.c_str(),SCOREP_USER_REGION_TYPE_COMMON)
 #endif
   }
   
   void traceEnd(const string& name)
   {
-#ifdef VTRACE
+#ifdef defined(VTRACE)
     VT_USER_END(name.c_str()) ;
+#elif defined(SCOREP)
+    SCOREP_USER_REGION_BY_NAME_END(name.c_str())
 #endif
   }
   
