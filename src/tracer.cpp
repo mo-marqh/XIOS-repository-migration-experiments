5b55980315da495f464ab14173f3a91915dcbc2f
ymipsl <ymipsl@d613d254-d18a-4304-b296-5125d4c4d670>
2019-03-15 16:02:22 +0000

    Implement tracing with ITAC (intel trace analyser)
    YM
    
    git-svn-id: https://forge.ipsl.fr/ioserver/svn/XIOS/trunk@1657 d613d254-d18a-4304-b296-5125d4c4d670

--- src/tracer.cpp
+++ src/tracer.cpp
@@ -8,20 +8,31 @@
 
 #include <scorep/SCOREP_User.h>
 
+#elif defined(ITAC)
+
+#include <VT.h>
+
 #endif
 
 #include <string>
+#include <map>
+#include <iostream>
 
 namespace xios
 {
   using namespace std ;
+
+  std::map<std::string,int> regionId ;
+  int count=0 ;
   
   void traceOn(void)
   {
 #if defined(VTRACE)
     VT_ON() ;
 #elif defined(SCOREP)
-    SCOREP_RECORDING_ON()
+    SCOREP_RECORDING_ON() ;
+#elif defined(ITAC)
+    VT_traceon() ;
 #endif
   }
   
@@ -31,6 +42,8 @@ namespace xios
     VT_OFF() ;
 #elif defined(SCOREP)
     SCOREP_RECORDING_OFF()
+#elif defined(ITAC)
+    VT_traceoff()   ;  
 #endif
   }
   
@@ -40,18 +53,48 @@ namespace xios
     VT_USER_START(name.c_str()) ;
 #elif defined(SCOREP)
     SCOREP_USER_REGION_BY_NAME_BEGIN(name.c_str(),SCOREP_USER_REGION_TYPE_COMMON)
+
+#elif defined(ITAC)
+    int classhandle ;
+    auto it = regionId.find(name);
+    if (it==regionId.end())
+    {
+      classhandle=count ;
+      count++ ;
+      VT_symdef (classhandle, name.c_str(), "XIOS") ;
+      regionId[name]=classhandle;
+    }
+    else classhandle = it->second ;
+    VT_begin(classhandle) ;
+    cout<<"VT_begin "<<name<<"  "<<classhandle<<endl ;
+
 #endif
+
   }
   
   void traceEnd(const string& name)
   {
-#ifdef defined(VTRACE)
+#if defined (VTRACE)
     VT_USER_END(name.c_str()) ;
 #elif defined(SCOREP)
     SCOREP_USER_REGION_BY_NAME_END(name.c_str())
+#elif defined(ITAC)
+    int classhandle ;
+    auto it = regionId.find(name);
+    if (it==regionId.end())
+    {
+      return ;
+      VT_classdef (name.c_str(), &classhandle) ;
+      regionId[name]=classhandle;
+    }
+    else classhandle = it->second ;
+    VT_end(classhandle) ;    
+    cout<<"VT_end "<<name<<"  "<<classhandle<<endl ;
+
 #endif
   }
   
+   
 //  void marker(const string& name,const string& text) ;
   
   
