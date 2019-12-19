4adbc5abbcbd309acb5cc8fcb6a64b61a87340a4
ymipsl <ymipsl@d613d254-d18a-4304-b296-5125d4c4d670>
2019-12-19 13:21:15 +0000

    XIOS test suite : files for test suite runs
    YM
    
    git-svn-id: https://forge.ipsl.fr/ioserver/svn/XIOS/trunk@1806 d613d254-d18a-4304-b296-5125d4c4d670

---
+++ xios_test_suite/TEST_SUITE/job_irene.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+######################
+## CURIE   TGCC/CEA ##
+######################
+#MSUB -r XIOS
+#MSUB -o client_output.out    # standard output
+#MSUB -e client_error.err    #error output
+#MSUB -eo
+#MSUB -c 1
+#MSUB -n 48  # Number of MPI tasks (SPMD case) or cores (MPMD case)
+#MSUB -X 
+#MSUB -x 
+#MSUB -T 1800      # Wall clock limit (seconds)
+#MSUB -q skylake           # thin nodes
+#MSUB -A gen0826
+#MSUB -Q test
+#MSUB -m work
+
+
+#============================= X64_IRENE_prod =============================
+
+
+cd $BRIDGE_MSUB_PWD
+
+module load subversion
+export arch=X64_IRENE
+export mode=prod
+
+./run_test
+
+
+#============================= X64_IRENE_debug =============================
+
+
+cd $BRIDGE_MSUB_PWD
+
+module load subversion
+export arch=X64_IRENE
+export mode=debug
+
+./run_test
+
+
+
+
