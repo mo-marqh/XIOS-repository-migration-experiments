565905a588b94a03548dd2fdbaf7cc488f78871a
ymipsl <ymipsl@d613d254-d18a-4304-b296-5125d4c4d670>
2019-12-20 13:57:22 +0000

    XIOS test suite : update jean-zay configuration YM
    git-svn-id: https://forge.ipsl.fr/ioserver/svn/XIOS/trunk@1822 d613d254-d18a-4304-b296-5125d4c4d670

--- xios_test_suite/COMPILE/job_compile_jeanzay.sh
+++ xios_test_suite/COMPILE/job_compile_jeanzay.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-##SBATCH --nodes=2
-#SBATCH --ntasks=8              # Nombre total de processus MPI
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks=1              # Nombre total de processus MPI
+#SBATCH --cpu-per-task=40
 #SBATCH --hint=nomultithread
 #SBATCH -t 00:30:00              # Temps elapsed maximum d'exécution
 #SBATCH -o output.out     # Nom du fichier de sortie
 #SBATCH -e error.err     # Nom du fichier d'erreur
-##SBATCH --account=psl@cpu
 #SBATCH -A psl@cpu
 
 #----------------- Compile job for Jean-Zay --------------
